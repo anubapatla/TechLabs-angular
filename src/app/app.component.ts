a55aaf4306cf1da03ed7e9a37b6c1b448c36ef5d
SriChatala <sri.chatala@finning.com>
2025-08-28 00:11:27 -0400

    sum of two numbers with inerpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TechLabs-angular';
+  title = 'TechLabs-angular 251';
+  a=10;
+  b=3;
+  ans = this.a+this.b;
 }
